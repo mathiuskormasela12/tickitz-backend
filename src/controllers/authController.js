867115ee57770fb04b0fb2be9604f8704f6c5b85
Mathius Kormasela <mathiuskormasela12@gmail.com>
2021-01-23 18:39:27 +0700

    solve auth mistake

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,7 +19,7 @@ exports.login = async (req, res) => {
 
     if (!isExist || !(await bcrypt.compare(req.body.password, isExist[0].password))) {
       return response(res, 400, false, 'Wrong email or password')
-    } else if (!isExist[0].activated) {
+    } else if (!isExist[0].activated && isExist[0].role === 'user') {
       return response(res, 400, false, "Your account haven't active")
     } else {
       const data = {
@@ -50,14 +50,16 @@ exports.register = async (req, res) => {
         if (!results) {
           return response(res, 400, false, 'Failed to register')
         } else {
-          const title = 'Register Successfully'
-          const message = `<div>
-            <h2>Please activeted your account, with the link below :</h2>
-            <p>
-              <a href="${process.env.APP_URL}/auth/active?id=${results.id}&email=${req.body.email}">Active Now !</a> 
-            </p>
-          </div>`
-          sendMail(req.body.email, title, message)
+          if (req.body.role !== 'admin') {
+            const title = 'Register Successfully'
+            const message = `<div>
+              <h2>Please activeted your account, with the link below :</h2>
+              <p>
+                <a href="${process.env.APP_URL}/auth/active?id=${results.id}&email=${req.body.email}">Active Now !</a> 
+              </p>
+            </div>`
+            sendMail(req.body.email, title, message)
+          }
           return response(res, 200, true, 'Register successfully', results)
         }
       } catch (err) {
